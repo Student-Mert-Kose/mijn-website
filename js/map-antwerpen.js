8e4193198c403e6de76a3db49b9379a914c316c9
Student-Mert-Kose <SkyMatrixMan@outlook.com>
2025-05-12 22:47:20 +0200

    .

--- js/map-antwerpen.js
+++ js/map-antwerpen.js
@@ -1,6 +1,14 @@
-let mapAnt = L.map('locatie').setView([51.2211, 4.3997], 12)
+let mapAnt = L.map('locatie').setView([51.2211, 4.3997], 11)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mapAnt);
+
+let circle = L.circle([51.2211,4.3997], {
+    color: '#red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 2000
+}).addTo(mapAnt);
+
